affc4792ea9f76070ce813e9a17f4af25ecfa8b7
noman <nomannabil30@gmail.com>
2022-11-08 06:38:17 +0600

    Server started successfully

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+// Test route
+app.get('/', (req, res) => {
+	res.send('Server is Running');
+});
+
+// Start server
+app.listen(port, () => {
+	console.log(`Server is running on port:...${port}`);
+});
